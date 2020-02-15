d62d7e077176097573614717f91f5fc969d3dc8d
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-02-15 16:38:56 +0700

    Initial commit

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameSystem : MonoBehaviour
+{
+    private int score = 0;
+    private bool isPlayerWin = false;
+    
+    public int winningScore = 500;
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerWin)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.green, 0.5f);
+
+                if (hit.transform.GetComponent<Mole>().IsAlive())
+                {
+                    hit.transform.GetComponent<Mole>().Dead();
+                    score += 100;
+                    scoreText.text = $"Score: {score}";
+
+
+                    Debug.Log($"current score {score}");
+
+                    if (score == winningScore)
+                    {
+                        isPlayerWin = true;
+                        Debug.Log("game ends");
+
+                        StartCoroutine(nameof(WinGame));
+                    }
+                }
+                    
+            }
+            else
+            {
+                Debug.DrawRay(ray.origin, ray.direction*100, Color.red, 0.5f);
+            }
+            
+        }
+    }
+    
+    private IEnumerator WinGame()
+    {
+        yield return new WaitForSeconds(.5f);
+        
+        SceneManager.LoadScene("WinningScene");
+    }
+
+}
---
+++ Assets/Scripts/Mole.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour
+{
+    private bool _dead = false;
+    private Animator _animator;
+    private AudioSource _audioSource;
+
+    public AudioClip deadSound;
+    public AudioClip fleeSound;
+    public AudioClip giggleSound;
+
+    public void Start()
+    {
+        _animator = transform.GetComponent<Animator>();
+        _audioSource = transform.GetComponent<AudioSource>();
+    }
+
+    public bool IsAlive() => !_dead;
+    
+    public void Dead()
+    {
+        if (_dead)
+        {
+            return;
+        }
+        
+        _dead = true;
+        
+        _animator.SetBool("isDead", true);
+        _audioSource.clip = deadSound;
+        _audioSource.Play();
+
+        StartCoroutine(nameof (Dying));
+    }
+
+    private IEnumerator Dying()
+    {
+        yield return new WaitForSeconds(.5f);
+        
+        transform.gameObject.SetActive(false);
+    }
+}
